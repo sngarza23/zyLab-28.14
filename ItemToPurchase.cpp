b7267193fcc943efcef4d62b05141051b64dc5cf
chiarates <167131843+chiarates@users.noreply.github.com>
2024-04-22 11:31:22 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+#include "ItemToPurchase.h"
+
+ItemToPurchase::ItemToPurchase() {
+    itemName = "none";
+    itemPrice = 0;
+    itemQuantity = 0;
+// Parameterized constructor to assign item name, item description, item price, and item quantity (default values of 0). (1 pt)
+// Public member functions
+// SetDescription() mutator & GetDescription() accessor (2 pts)
+// PrintItemCost() - Outputs the item name followed by the quantity, price, and subtotal
+// PrintItemDescription() - Outputs the item name and description
+// Private data members
+// string itemDescription - Initialized in default constructor to "none"
