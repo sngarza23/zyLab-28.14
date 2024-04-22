79494e257075dcd5c1eac5b5ad162b3f589bd9e0
chiarates <167131843+chiarates@users.noreply.github.com>
2024-04-22 11:34:12 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 using namespace std;
-
 #include "ItemToPurchase.h"
-
-ItemToPurchase::ItemToPurchase() {
-    itemName = "none";
-    itemPrice = 0;
-    itemQuantity = 0;
-// Parameterized constructor to assign item name, item description, item price, and item quantity (default values of 0). (1 pt)
+// Parameterized constructor to assign item name, item description, item price, and item quantity (default values of 0).
+ItemToPurchase::ItemToPurchase(string& itemName, string& itemDescription, int itemPrice, int itemQuantity) {
 // Public member functions
+this->itemName = itemName;
+this->itemDescription = itemDescription;
+this->itemPrice = itemPrice;
+this->itemQuantity = itemQuantity;
+}
+
 // SetDescription() mutator & GetDescription() accessor (2 pts)
 // PrintItemCost() - Outputs the item name followed by the quantity, price, and subtotal
 // PrintItemDescription() - Outputs the item name and description
