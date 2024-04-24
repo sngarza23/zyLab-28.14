092c70372dff89d731bbc6a537551ee0d408e2f2
sngarza23 <167341026+sngarza23@users.noreply.github.com>
2024-04-24 11:19:50 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,17 +5,53 @@ using namespace std;
 #include "ShoppingCart.h"
 
 void PrintMenu() {
-   /* Type your code here */
-   
+  cout << " MENU" << endl;
+   cout << "a - Add item to cart" << endl;
+   cout << "d - Remove item from cart" << endl;
+   cout << "c - Change item quantity" << endl;
+   cout << "i - Output shopping cart" << endl;
+   cout << "q - Quit" << endl;
+   cout << endl;
+   cout << "Choose an option: " << endl;
 }
 
 void ExecuteMenu(char option, ShoppingCart& theCart) {
-   /* Type your code here */
-   
+   switch(option) {
+      case 'a':
+         break;
+      case 'd':
+         break;
+      case 'c':
+         break;
+      case 'i':
+         break;
+      case 'q':
+         break;
+      default:
+      cout << "No go again." << endl;
+   }
 }
 
 int main() {
-   /* Type your code here */
+   string customerName, currerntDate;
+   cout << "Enter customer's name: ";
+   getline(cin, customerName);
+   cout << "Enter today's date: ";
+   getline(cin, currentDate);
+   cout <<"Customer name: " << customerName << endl;
+   cout << "Today's date: " << currentDate << endl;
+
+   ShoppingCart cart(customerName, currentDate);
+
+   char choice;
+   do { PrintMenu();
+       cout <<"Choose an option: ";
+       cin >> choice;
+       cin.ignore();
+
+       ExecuteMenu(choice, cart);
+
+      } while (choice != 'q');
    
    return 0;
 }
