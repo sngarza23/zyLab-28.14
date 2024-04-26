980679c0180ed01789df6f41a1ee84b8c494fa55
chiarates <167131843+chiarates@users.noreply.github.com>
2024-04-25 20:48:41 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,16 @@
 //prompt the user for a customer's name and today's date. Output the name and date. Create an object of type ShoppingCart
 
 #include <iostream>
-#include <iomanip>
-using namespace std;
-
+#include <string>
 #include "ShoppingCart.h"
 
+using namespace std;
+
+
 void PrintMenu() {
-   cout << " MENU" << endl;
    cout << "a - Add item to cart" << endl;
-   cout << "d - Remove item from cart" << ednl;
-   cout << "c - Change item quantity" << ednl;
+   cout << "d - Remove item from cart" << endl;
+   cout << "c - Change items' quantity" << endl;
    cout << "i - Output shopping cart" << endl;
    cout << "q - Quit" << endl;
    cout << endl;
@@ -27,8 +27,17 @@ void ExecuteMenu(char option, ShoppingCart& theCart) {
       case 'c':
          break;
       case 'i':
+         theCart.PrintDescriptions();
+         break;
+      case 'o':
+         cout << "OUTPUT SHOPPING CART" << endl;
+         cout << theCart.GetCustomerName() << "'s Shopping Cart - " << theCart. GetDate() << endl;
+         cout<< "Number of Items: " << theCart.GetNumItemsInCart() << endl;
+         theCart.PrintDescriptions();
+         theCart.PrintTotal();
          break;
       case 'q':
+      cout << "Exiting program" << endl;
          break;
       default:
       cout << "No go again." << endl;
@@ -36,18 +45,22 @@ void ExecuteMenu(char option, ShoppingCart& theCart) {
 }
 
 int main() {
-   string customerName, currerntDate;
-   cout << "Enter customer's name: ";
+   string customerName, currentDate;
+   cout << "Enter customer's name:" << endl;
    getline(cin, customerName);
-   cout << "Enter today's date: ";
+   
+   cout << "Enter today's date:" << endl;
    getline(cin, currentDate);
-   cout <<"Customer name: " << customer Name << endl;
-   cout << Today's date: " << currentDate << endl;
+   
+   cout <<"Customer name: " << customerName << endl;
+   cout << "Today's date: " << currentDate << endl;
 
    ShoppingCart cart(customerName, currentDate);
 
    char choice;
-   do { PrintMenu();
+   
+   do { 
+      PrintMenu();
        cout <<"Choose an option: ";
        cin >> choice;
        cin.ignore();
