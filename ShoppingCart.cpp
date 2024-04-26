3ea84d72c4c55e656bff781aced5ec1f49e8e71d
chiarates <167131843+chiarates@users.noreply.github.com>
2024-04-25 20:49:10 -0500

    Update ShoppingCart.cpp

--- ShoppingCart.cpp
+++ ShoppingCart.cpp
@@ -1,28 +1,96 @@
-// Default constructor
-// Parameterized constructor which takes the customer name and date as parameters (1 pt)
-// Private data members
-// string customerName - Initialized in default constructor to "none"
-// string currentDate - Initialized in default constructor to "January 1, 2016"
-// vector < ItemToPurchase > cartItems
-// Public member functions
-// GetCustomerName() accessor (1 pt)
-// GetDate() accessor (1 pt)
-// AddItem()
-// Adds an item to cartItems vector. Has parameter ItemToPurchase. Does not return anything.
-// RemoveItem()
-// Removes item from cartItems vector. Has a string (an item's name) parameter. Does not return anything.
-// If item name cannot be found, output a message: Item not found in cart. Nothing removed.
-// ModifyItem()
-// Modifies an item's description, price, and/or quantity. Has parameter ItemToPurchase. Does not return anything.
-// If item can be found (by name) in cart, check if parameter has default values for description, price, and quantity. If not, modify item in cart.
-// If item cannot be found (by name) in cart, output a message: Item not found in cart. Nothing modified.
-// GetNumItemsInCart() (2 pts)
-// Returns quantity of all items in cart. Has no parameters.
-// GetCostOfCart() (2 pts)
-// Determines and returns the total cost of items in cart. Has no parameters.
-// PrintTotal()
-// Outputs total of objects in cart.
-// If cart is empty, output a message: SHOPPING CART IS EMPTY
-// PrintDescriptions()
-// Outputs each item's description.
-// If cart is empty, output a message: SHOPPING CART IS EMPTY
+#include <iostream>
+#include <algorithm>
+#include "ShoppingCart.h"
+using namespace std;
+
+#include "ItemToPurchase.h"
+
+ShoppingCart::ShoppingCart() {
+  customerName = "none";
+  currentDate = "January 1, 2016";
+  }
+ShoppingCart::ShoppingCart(string name, string date) {
+  customerName = name;
+  currentDate = date;
+  }
+string ShoppingCart::GetCustomerName() const {
+        return customerName;
+    }
+string ShoppingCart::GetDate() const {
+        return currentDate;
+    }
+void ShoppingCart::AddItem( ItemToPurchase item) {
+        cartItems.push_back(item);
+    }
+void ShoppingCart::RemoveItem(string itemName) {
+        auto it = find_if(cartItems.begin(), cartItems.end(),
+                           [&itemName](const ItemToPurchase& item) {
+                               return item.GetName() == itemName;
+                           });
+      if (it != cartItems.end()) {
+            cartItems.erase(it);
+      } else {
+            cout << "Item not found in cart. Nothing removed." << endl;
+      }
+    }
+
+void ShoppingCart::ModifyItem(ItemToPurchase newItem) {
+        auto it = find_if(cartItems.begin(), cartItems.end(),
+                           [&newItem](const ItemToPurchase& item) {
+                               return item.GetName() == newItem.GetName();
+                          });
+        if (it != cartItems.end()) {
+            if (newItem.GetDescription() != "") {
+                it->SetDescription(newItem.GetDescription());
+            }
+            if (newItem.GetPrice() != 0) {
+                it->SetPrice(newItem.GetPrice());
+            }
+            if (newItem.GetQuantity() != 0) {
+                it->SetQuantity(newItem.GetQuantity());
+            }
+        } else {
+            cout << "Item not found in cart. Nothing modified." << endl;
+        }
+    }
+
+int ShoppingCart::GetNumItemsInCart()  {
+        int totalQuantity = 0;
+    for (const auto& item : cartItems) {
+            totalQuantity += item.GetQuantity();
+    }
+        return totalQuantity;
+}
+
+double ShoppingCart::GetCostOfCart() {
+    int totalCost = 0;
+      for (const auto& item : cartItems) {
+          totalCost += item.GetQuantity() * item.GetPrice();
+      }
+        return totalCost;
+}
+
+void ShoppingCart::PrintTotal()  {
+    if (cartItems.empty()) {
+            cout << "SHOPPING CART IS EMPTY" << endl;
+    } else {
+            cout << "Total: $"  << GetCostOfCart() << endl;
+    }
+}
+
+void ShoppingCart::PrintDescriptions()  {
+    if (cartItems.empty()) {
+          cout << "SHOPPING CART IS EMPTY" << endl;
+    } else {
+          cout << "Item Descriptions" << endl;
+            for (const auto& item : cartItems) {
+                cout << item.GetDescription() << endl;
+            }
+        }
+    }
+
+// private:
+//     string customerName;
+//     string currentDate;
+//     vector<ItemToPurchase> cartItems;
+//};
