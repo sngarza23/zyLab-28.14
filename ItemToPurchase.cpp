e6fc15d48deaf41fe99267e860e763d6721ea601
chiarates <167131843+chiarates@users.noreply.github.com>
2024-04-30 23:39:47 -0500

    Update ItemToPurchase.cpp

--- ItemToPurchase.cpp
+++ ItemToPurchase.cpp
@@ -1,47 +1,54 @@
-#include "ItemToPurchase.h"
 #include <iostream>
-
-ItemToPurchase::ItemToPurchase() : itemName("none"), itemDescription("none"), itemPrice(0), itemQuantity(0) {}
-
-ItemToPurchase::ItemToPurchase(const std::string& name, const std::string& description, int price, int quantity)
-    : itemName(name), itemDescription(description), itemPrice(price), itemQuantity(quantity) {}
-
-void ItemToPurchase::SetName(const std::string& name) {
-    itemName = name;
+#include <string>
+#include "ItemToPurchase.h"
+using namespace std;
+ItemToPurchase::ItemToPurchase() {
+itemName = "none";
+itemDescription = "none";
+itemPrice = 0;
+itemQuantity = 0;
+return;
 }
-
-void ItemToPurchase::SetDescription(const std::string& description) {
-    itemDescription = description;
+ItemToPurchase::ItemToPurchase(string name, string description, int price, int quantity) {
+itemName = name;
+itemDescription = description;
+itemPrice = price;
+itemQuantity = quantity;
+return;
 }
-
+void ItemToPurchase::SetName(string name) {
+itemName = name;
+return;
+}
+string ItemToPurchase::GetName() const {
+return itemName;
+}
+void ItemToPurchase::SetDescription(string description) {
+itemDescription = description;
+return;
+}
+string ItemToPurchase::GetDescription() const {
+return itemDescription;
+}
 void ItemToPurchase::SetPrice(int price) {
-    itemPrice = price;
+itemPrice = price;
+return;
 }
-
-void ItemToPurchase::SetQuantity(int quantity) {
-    itemQuantity = quantity;
-}
-
-std::string ItemToPurchase::GetName() const {
-    return itemName;
-}
-
-std::string ItemToPurchase::GetDescription() const {
-    return itemDescription;
-}
-
 int ItemToPurchase::GetPrice() const {
-    return itemPrice;
+return itemPrice;
 }
-
+void ItemToPurchase::SetQuantity(int quantity) {
+itemQuantity = quantity;
+return;
+}
 int ItemToPurchase::GetQuantity() const {
-    return itemQuantity;
+return itemQuantity;
 }
-
 void ItemToPurchase::PrintItemCost() const {
-    std::cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " = $" << (itemQuantity * itemPrice) << std::endl;
+cout << itemName << " " << itemQuantity << " @ $" << itemPrice << " = $" << itemQuantity * itemPrice << endl;
+return;
 }
-
 void ItemToPurchase::PrintItemDescription() const {
-    std::cout << itemName << ": " << itemDescription << std::endl;
+cout << itemName << ": " << itemDescription << endl;
+return;
 }
