bc98deebd1b26ed072fb4277ee4357ffa5eb8fb6
sngarza23 <167341026+sngarza23@users.noreply.github.com>
2024-04-30 23:38:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // https://github.com/sngarza23/zyLab-28.14.git
-//partner Chiara Tesoriero
+//partner Chiara Tesoriero and Sam Garza
 #include <iostream>
 #include <iomanip>
 #include "ShoppingCart.h"
